54a22a4877948828376876e6bdf049e7e322ed5b
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-19 13:48:16 -0500

    Update GameLinkedList.java

--- mod/src/GameLinkedList.java
+++ mod/src/GameLinkedList.java
@@ -1,3 +1,22 @@
-public class GameLinkedList{
+/**
+ * Maintains ordered linked list of Nodes
+ * @author Patrick McKnew
+ *
+ */
+public class GameLinkedList implements GameLinkedListInterface {
+	private String i;
+	private int nodeScore;
+	Node node = new Node();
 
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeLowest() {
+		// TODO Auto-generated method stub
+
+	}
 }
