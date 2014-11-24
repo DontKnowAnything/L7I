64b592b794d467a7d7c2b149e6c5ca9f8687e133
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-24 15:40:52 -0500

    Update Node.java

--- mod/src/Node.java
+++ mod/src/Node.java
@@ -5,18 +5,18 @@
  * @author Patrick McKnew
  */
 public class Node {
-	private String s;
-	private int score;
-	private Node next;
+   private String s;
+   private int score;
+   private Node next;
 
 	/**
 	 *
 	 */
-	public Node() {
-		s = "";
-		score = 0;
-		next = null;
-	}
+   public Node() {
+      s = "";
+      score = 0;
+      next = null;
+   }
 
 	/**
 	 * The overloaded constructor
@@ -24,65 +24,70 @@ public class Node {
 	 * @param sconstructor String for name
 	 * @param gameScore    The score
 	 */
-	public Node(String sconstructor, int gameScore) {
-		//sconstructor=getS();
-		setS(sconstructor);
-		setScore(gameScore);
-		next = null;
-	}
+   public Node(String sconstructor, int gameScore, Node n) {
+   	//sconstructor=getS();
+      setS(sconstructor);
+      setScore(gameScore);
+      setNext(n);
+   }
 
 	/**
 	 * @return Name of Player
 	 */
-	public String getS() {
-		String gs = s;
-
-		return gs;
-
-
-	}
+   public String getS() {
+      String gs = s;
+   
+      return gs;
+   
+   
+   }
 
 	/**
 	 * @param sS Player name
 	 */
-	public void setS(String sS) {
-
-		String mS = sS;
-		if (!(mS.equals(""))) {
-			s = mS;
-		} else {
-
-		}
-
-	}
+   public void setS(String sS) {
+   
+      String mS = sS;
+      if (!(mS.equals(""))) {
+         s = mS;
+      } 
+      else {
+      System.err.println("Player name is empty");
+      }
+   
+   }
 
 	/**
 	 * @return Score for player
 	 */
-	public int getScore() {
-		int playerScore = score;
-		return playerScore;
-	}
+   public int getScore() {
+      int playerScore = score;
+      return playerScore;
+   }
 
 	/**
 	 * @param aScore Score for player
 	 */
-	public void setScore(int aScore) {
-		int ss = aScore;
-		score = ss;
-	}
-
+   public void setScore(int aScore) {
+      int ss = aScore;
+      if(ss>0){
+         score = ss;
+      }
+      else{
+         System.err.println("Score less then 0");
+      }
+   }
 	/**
 	 * @return The Node
 	 */
-	public Node getNext() {
-		return next;
-	}
+   public Node getNext() {
+      return next;
+   }
 
 	/**
 	 * @param nextNode The next node is GameLinkedList
 	 */
-	public void setNext(Node nextNode) {
-		next = nextNode;
-	}
+   public void setNext(Node nextNode) {
+      next = nextNode;
+   }
 }
