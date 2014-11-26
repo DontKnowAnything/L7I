9fca4662d1b7768b0732bbf2d527021eba87c2c8
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-25 20:29:35 -0500

    Update GameLinkedList.java

--- mod/src/GameLinkedList.java
+++ mod/src/GameLinkedList.java
@@ -18,7 +18,6 @@ public class GameLinkedList implements GameLinkedListInterface {
    public GameLinkedList( String n, int nodeS ) {
       name = n;
       nodeScore = nodeS;
-   
    }
 
    @Override
@@ -38,7 +37,7 @@ public class GameLinkedList implements GameLinkedListInterface {
    public void removeLowest() {//Beginning VS End : Depends on ascending or descending?
    	// TODO Auto-generated method stub
       Node lowestScore = new Node();
-   
+      
    	//Remove from end
       Node previousNode = null;
       Node currentNode = head;
@@ -55,43 +54,35 @@ public class GameLinkedList implements GameLinkedListInterface {
          else {
             err += "wrong";
          }
-      
-      
       }
    }
 
    public void add( String newName, int newScore ) {
       Node newNode = new Node( newName, newScore, head );
       
-      if(newScore>=0&&(!(newName.equals("")))){
+      if( newScore >= 0 && ( !( newName.equals("") ) ) ){
          if ( isEmpty() ) {//List may be empty
-         //newNode.setScore(newScore);
-         //Node newNode= new Node();
-         
             newNode.setNext( head );
             head = newNode;
          }
          else{
-            //newNode.setScore(newScore);
-         //System.err.print("Error");
-         
-         
-         //} else {
-            if ( head.getScore() > newNode.getScore() ) {//Node belongs at beginning, change to > and
+            if ( head.getScore() > newNode.getScore() ) {//Node belongs at beginning, change to < and
                Node current = head;
                newNode.setNext( current );
-               head = newNode;//Works
+               head = newNode;
             } 
             else {
-               Node current = head;//Node belongs at end
-               
-               while ( current.getNext() != null && current.getNext().getScore() < newNode.getScore() ) {//to < for lowest to highest
+               Node current = head;//Node belongs at end 
+               while ( current.getNext() != null && current.getNext().getScore() < newNode.getScore() ) {//to > for highest to lowest
                   current = current.getNext();
                }
                newNode.setNext( current.getNext() );
                current.setNext( newNode );
             }
          }
+      }
+      else{
+         System.err.println( "Node not entered" );
       }
    }
    public String toString() {
