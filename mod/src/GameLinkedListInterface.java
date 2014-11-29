17f35e7fc172528b1a72f6da445b8dd46a17ba03
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-11-29 11:09:12 -0500

    Update GameLinkedListInterface.java

--- mod/src/GameLinkedListInterface.java
+++ mod/src/GameLinkedListInterface.java
@@ -1,16 +1,16 @@
 public interface GameLinkedListInterface {
-	public int MAXSIZE = 10;
+   public int MAXSIZE = 10;
 
-	public boolean isEmpty();
+   public boolean isEmpty();
 
-	public void removeLowest();
+   public void removeLowest();
 
-	public String toString();
+   public String toString();
 
 // Remove the comment for 1 of the following add methods
 
 	// Option 1
-//public void add(String newName, int newScore);
+   public void add(String newName, int newScore);
 
 	// Option 2
 // public void add(String newName, int newScore) throws LinkedListException;
