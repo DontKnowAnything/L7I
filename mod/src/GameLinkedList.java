e643ff650648da02c9f796b296a2d92f0c9892d3
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-12-01 15:20:40 -0500

    Update GameLinkedList.java

--- mod/src/GameLinkedList.java
+++ mod/src/GameLinkedList.java
@@ -1,6 +1,5 @@
 /**
- * Maintains ordered linked list of Nodes and throws error message for invalid
- * Node
+ * Maintains ordered linked list of Nodes and throws error message for invalid Node
  *
  * @author Patrick McKnew
  */
@@ -8,23 +7,17 @@ public class GameLinkedList implements GameLinkedListInterface {
    private String name;
    private int count;
    private int nodeScore;
-   private Node head;// first
+   private Node head;//first
+   private Node tail;//last
 
-	/**
-	 * 
-	 */
    public GameLinkedList() {
       name = "";
-      count = 0;
+      count=0;
       nodeScore = 0;
       head = null;
    }
 
-	/**
-	 * @param n
-	 * @param nodeS
-	 */
-   public GameLinkedList(String n, int nodeS) {
+   public GameLinkedList( String n, int nodeS ) {
       name = n;
       nodeScore = nodeS;
    }
@@ -43,70 +36,70 @@ public class GameLinkedList implements GameLinkedListInterface {
    }
 
    @Override
-   public void removeLowest() {// Beginning VS End : Depends on ascending or
-   							// descending?
+   public void removeLowest() {//Beginning VS End : Depends on ascending or descending?
    	// TODO Auto-generated method stub
-   
-   	// Remove from end
+      Node lowestScore = new Node();
+      
+   	//Remove from end
       Node previousNode = null;
       Node currentNode = head;
       String err = "";
    
       if (currentNode == null) {
-         err += "t";
-      }
-      if (previousNode == null) {
-         head = currentNode.getNext();
-         currentNode = null;
-      }
-      count--;
-   }
-   @Override
-   public void add(String newName, int newScore ) throws LinkedListException {
-      Node newNode = new Node(newName, newScore, head);
-   
-      if (newScore >= 0 && (!(newName.equals("")))) {
-         if (isEmpty()) {// List may be empty
-            newNode.setNext(head);
-            head = newNode;
-            count++;
-         } 
-         else {
-            if (head.getScore() > newNode.getScore()) {// Node belongs at
-            											// beginning, change
-            											// to < and
-               Node current = head;
-               newNode.setNext(current);
-               head = newNode;
-               count++;
-               if (count > MAXSIZE) {
-                  removeLowest();
-               }
-            } 
-            else {
-               Node current = head;// Node belongs at end
-               while (current.getNext() != null
-               		&& current.getNext().getScore() < newNode
-               				.getScore()) {// to > for highest to lowest
-                  current = current.getNext();
-               }
-               count++;
-            
-               newNode.setNext(current.getNext());
-               current.setNext(newNode);
-            	// if( count > 4 ){
-               if (count > MAXSIZE) {
-                  removeLowest();
-               
-               }
-            }
-         }
+         // err += "t";
       } 
       else {
-         throw new LinkedListException ("");
+         if (previousNode == null) {
+            head = currentNode.getNext();
+            currentNode = null;
+            //count--;
+         } 
+         //else if(count==MAXSIZE) {
+            //System.err.println("10");
+         //}
       }
    }
 
+   public void add( String newName, int newScore )throws LinkedListException {
+      Node newNode = new Node( newName, newScore, head );
+      try{
+         if( newScore >= 0 && ( !( newName.equals("") ) ) ){
+            if ( isEmpty() ) {//List may be empty
+               newNode.setNext( head );
+               head = newNode;
+               count++;
+            }
+            else{
+               if ( head.getScore() > newNode.getScore() ) {//Node belongs at beginning, change to < and
+                  Node current = head;
+                  newNode.setNext( current );
+                  head = newNode;
+                  count++;
+               } 
+               else {
+                  Node current = head;//Node belongs at end 
+                  while ( current.getNext() != null && current.getNext().getScore() < newNode.getScore() ) {//to > for highest to lowest
+                     current = current.getNext();
+                  }
+                  newNode.setNext( current.getNext() );
+                  current.setNext( newNode );
+                  count++;
+                  
+                  if( count >= 5 ){
+                     removeLowest();
+                     //count--;
+                  }
+               }
+            }
+         }
+         else{
+         
+         }
+      }
+      catch (LinkedListException i){
+         throw new LinkedListException("Node error");
+      }
+   }
    public String toString() {
       Node b = head;
       String s = "";
