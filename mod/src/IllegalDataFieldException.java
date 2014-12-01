6d70748cb36118564596eefa3d6c91ea45ef5090
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-12-01 08:41:33 -0500

    Update IllegalDataFieldException.java

--- mod/src/IllegalDataFieldException.java
+++ mod/src/IllegalDataFieldException.java
@@ -1,3 +1,20 @@
-public class IllegalDataFieldException{
+/**
+ * Reports Node errors to GameLinkedList class <br>
+ * Name is null or blank<br>
+ * Score is less then 0
+ * 
+ * @author Patrick McKnew
+ *
+ */
+public class IllegalDataFieldException extends RuntimeException {
+
+	/**
+	 * @param message
+	 */
+   public IllegalDataFieldException(String message) {
+      super(message);
+   	// TODO Auto-generated constructor stub
+   }
+	
 
 }
