4f39218a940153971a67ab5adf16199a20f7975a
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-12-03 12:02:26 -0500

    Update GameLinkedList.java

--- mod/src/GameLinkedList.java
+++ mod/src/GameLinkedList.java
@@ -10,6 +10,9 @@ public class GameLinkedList implements GameLinkedListInterface {
 	private int nodeScore;
 	private Node head;//first
 
+	/**
+	 * 
+	 */
 	public GameLinkedList() {
 		name = "";
 		count = 0;
@@ -20,11 +23,15 @@ public class GameLinkedList implements GameLinkedListInterface {
 	/**
 	 * @param n     The name
 	 * @param nodeS The score
-	 * @throws LinkedListException if
 	 */
 	public GameLinkedList(String n, int nodeS) {
+		try{
 		name = n;
 		nodeScore = nodeS;
+		}
+		catch(IllegalDataFieldException dfe){
+			dfe.getMessage();
+		}
 
 	}
 
@@ -68,45 +75,52 @@ public class GameLinkedList implements GameLinkedListInterface {
 
 		Node newNode = new Node(newName, newScore, head);
 
-		if (newScore >= 0 && (!(newName.equals("")))) {
-			if (isEmpty()) {//List may be empty
-				newNode.setNext(head);
-				head = newNode;
-				count++;
-			} else {
-				if (head.getScore() > newNode.getScore()) {//Node belongs at beginning, change to < and
-					Node current = head;
-					newNode.setNext(current);
-					head = newNode;
-					count++;
-					if (count > 5) {
-						removeLowest();
-					}
-				} else {
-					Node current = head;//Node belongs at end
-					while (current.getNext() != null && current.getNext().getScore() < newNode.getScore()) {//to > for highest to lowest
-						current = current.getNext();
-					}
-					count++;
+		
+			if (newScore >= 0 && (!(newName.equals("")))) {
+				try {
+					if (isEmpty()) {//List may be empty
+						newNode.setNext(head);
+						head = newNode;
+						count++;
+					} else {
+						if (head.getScore() > newNode.getScore()) {//Node belongs at beginning, change to < and
+							Node current = head;
+							newNode.setNext(current);
+							head = newNode;
+							count++;
+							if (count > 5) {
+								removeLowest();
+							}
+						} else {
+							Node current = head;//Node belongs at end
+							while (current.getNext() != null && current.getNext().getScore() < newNode.getScore()) {//to > for highest to lowest
+								current = current.getNext();
+							}
+							count++;
 
-					newNode.setNext(current.getNext());
-					current.setNext(newNode);
-					//if( count > 4 ){
-					if (count > 5) {
-						removeLowest();
+							newNode.setNext(current.getNext());
+							current.setNext(newNode);
+							//if( count > 4 ){
+							if (count > 5) {
+								removeLowest();
+							}
+						}
 					}
+				} catch (IllegalDataFieldException e) {
+					// TODO Auto-generated catch block
+					e.getMessage();
 				}
+			} else {
+				throw new LinkedListException("Node not entered");
 			}
-		} else {
-			throw new IllegalDataFieldException("");
-		}
-	}
+		} 
+	
 
 	public String toString() {
 		Node b = head;
 		String s = "";
 		while (b != null) {
-			s += b.getS() + " " + b.getScore() + "\n";
+			s += " Player "+b.getS() + " Score " + b.getScore() + "\n";
 			b = b.getNext();
 		}
 		return s;
